18748492bf4e00eb3d9b1e2b390a7ee5821670e0
Xoan Carreira <xoancano@gmail.com>
2024-02-01 14:09:50 +0100

    Incluir JS para resposta do formulario

---
+++ js/script.js
@@ -0,0 +1,31 @@
+const popupCerrar = document.getElementById("popupButoon");
+const popupContainer = document.getElementById("popupContainer");
+const textoPopup = document.querySelector(".popup__p");
+
+let nombre = document.getElementById("nombre");
+let mensaje = document.getElementById("mensaje");
+let apellidos = document.getElementById("apellidos");
+
+
+
+
+function verificar(){
+    if (nombre.value !== "" & apellidos.value !== "" & mensaje.value !== ""){
+        mostrar()
+    }else{
+        alert("Debes rellenar los campos indicados");
+    }
+}
+
+
+function mostrar() {
+    popupContainer.classList.toggle("ocultar");
+    textoPopup.textContent = `Hola ${nombre.value} ${apellidos.value}, gracias por el comentario (${mensaje.value}) pero esto solo es un formulario de muestra.`;
+}
+
+
+
+
+function cerrar() {
+    popupContainer.classList.toggle("ocultar");
+}
